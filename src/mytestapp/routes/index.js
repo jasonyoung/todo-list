da00e5f65fc4acae640dc4d9d92f5b74fb4cbfb3
Jason Young <youngjase82@gmail.com>
2013-10-13 14:00:40 +0100

    Added sample to-do items

--- src/mytestapp/routes/index.js
+++ src/mytestapp/routes/index.js
@@ -1,8 +1,22 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+	res.render('index', {
+		title: 'Express',
+		todos: [{
+			description: 'Buy eggs',
+			due: new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // 1 day from now
+			done: false
+		}, {
+			description: 'Write blog post',
+			due: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+			done: false
+		}, {
+			description: 'Build to-do list app',
+			due: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+			done: true
+		}]
+	});
 };
