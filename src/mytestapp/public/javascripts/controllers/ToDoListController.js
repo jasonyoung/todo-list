8d7ca4e01d154d96b1f50e15430e34ed51f1b94b
Jason Young <youngjase82@gmail.com>
2013-10-13 17:14:30 +0100

    Move todos to right list on update

---
+++ src/mytestapp/public/javascripts/controllers/ToDoListController.js
@@ -0,0 +1,15 @@
+function ToDoListController($scope) {
+	$scope.todos = [];
+
+	$scope.doneFilter = {
+		done: true
+	};
+
+	$scope.notDoneFilter = {
+		done: false
+	};
+
+	$scope.setTodos = function(todos) {
+		$scope.todos = todos;
+	};
+};
